965cb92aaa364862b8cb0f8a92c561a8fb9ed607
jeantristanb <jeantristanb@free.fr>
2023-06-05 13:29:09 +0200

    update pipeline

--- analyse_multigwas/bin/GetListChro.py
+++ analyse_multigwas/bin/GetListChro.py
@@ -1,9 +1,34 @@
 #!/usr/bin/env python3
 
+import gzip
 import sys
-import pandas as pd
 import argparse
-import numpy as np
+
+def spl_gz(z,sep=None) :
+   return z.decode('utf-8').replace('\n','').split(sep)
+
+def spl_nogz(z,sep=None) :
+   return z.replace('\n','').split(sep)
+
+
+def openf(File) :
+  def is_gz_file(filepath):
+      with open(filepath, 'rb') as test_f:
+        return test_f.read(2) == b'\x1f\x8b'
+  def checkexists(path_to_file) :
+    if exists(path_to_file)==False :
+     sys.exist('file '+ path_to_file+' doesn t exist')
+  balisegz=False
+  if is_gz_file(File) :
+    readf=gzip.open(File)
+    balisegz=True
+  else :
+    readf=open(File)
+  spl=spl_nogz
+  if balisegz:
+     spl=spl_gz
+  return (readf, balisegz,spl)
+
 
 def parseArguments():
     parser = argparse.ArgumentParser(description='extract list chro of list file')
@@ -16,13 +41,20 @@ args = parseArguments()
 
 listfile=args.list_file.split(',')
 ListChro=set([])
-for filea in  listfile :
- ReadGWAS=open(filea)
- Head2=ReadGWAS.readline().replace('\n','').split()
- PosHeadChr=Head2.index(args.head_chr)
- for lireGWAS in ReadGWAS :
-    ListChro.add(lireGWAS.split()[PosHeadChr])
- ReadGWAS.close()
+
+for File in  listfile :
+       (ReadGWAS, balisegz, spl)=openf(File)
+       for line in ReadGWAS :
+            Head2=spl(ReadGWAS.readline())#.replace('\n','').split()
+            if args.head_chr in Head2 :
+               PosHeadChr=Head2.index(args.head_chr)
+               nelem=len(Head2)
+               break
+       for lireGWAS in ReadGWAS :
+           tmp=spl(lireGWAS)
+           if len(tmp) == nelem and (args.head_chr not in tmp):
+              ListChro.add(tmp[PosHeadChr])
+       ReadGWAS.close()
 
 for x in ListChro :
    print(x)
